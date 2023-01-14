7de27bb8194cbec9e311ce42c9d01c0676073f97
Markus Mayr <markus.mayr@outlook.com>
2023-01-14 17:54:54 +0100

    Trying to push state version of NAS

--- hosts/nas/default.nix
+++ hosts/nas/default.nix
@@ -39,7 +39,7 @@
 
     time.timeZone = "Europe/Vienna";
 
-    system.stateVersion = "22.05"; # Did you read the comment?
+    system.stateVersion = "22.11"; # Did you read the comment?
 
     networking.hostName = "nas"; # Define your hostname.
     networking.useDHCP = false;
