8e78aaa438878a925fb512c2c9a14d58ce4e5eac
Markus Mayr <markus.mayr@outlook.com>
2023-03-01 06:47:05 +0100

    Enable automatic garbage-collection

--- bits/system/core.nix
+++ bits/system/core.nix
@@ -27,6 +27,11 @@
       extraOptions = ''
         experimental-features = nix-command flakes
       '';
+      gc = {
+        automatic = true;
+        dates = "weekly";
+        options = "--delete-older-than 14d";
+      };
     };
 
     users.mutableUsers = false;
