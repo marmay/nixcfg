604ab244cecef23c941e138bdaf3ec7f4b7ac967
Markus Mayr <markus.mayr@outlook.com>
2023-05-08 20:27:21 +0200

    Also update notebook config

--- hosts/notebook/default.nix
+++ hosts/notebook/default.nix
@@ -2,32 +2,11 @@
 
 {
   imports = [
-    ../../bits/system/autoupdate.nix
-    ../../bits/system/core.nix
-    ../../bits/system/data.nix
-    ../../bits/system/gnome.nix
-    ../../bits/system/intel.nix
-    ../../bits/system/nas_client.nix
-    ../../bits/system/opengl.nix
-    ../../bits/system/printing.nix
-    ../../bits/system/sound.nix
-    ../../bits/system/ssh.nix
-    ../../bits/system/steam.nix
-    ../../bits/system/xserver.nix
-    ../../users/markus/core.nix
-    ../../users/markus/admin.nix
-    ../../users/markus/local.nix
-    ../../users/markus/smallgui.nix
-    ../../users/markus/wingames.nix
-    ../../users/marion/full.nix
-    ../../users/raphaela/full.nix
     ./hardware.nix
   ];
 
   config = {
     system.stateVersion = "22.11";
-
-    time.timeZone = "Europe/Vienna";
 
     boot.loader.grub = {
       enable = true;
@@ -40,5 +19,16 @@
       hostName = "notebook";
       networkmanager.enable = true;
     };
+
+    marmar = {
+      nas_client = true;
+      intelGpuSupport = true;
+      printingSupport = true;
+      steam = true;
+    };
+
+    marmar.users.markus.enable = true;
+    marmar.users.marion.enable = true;
+    marmar.users.raphaela.enable = true;
   };
 }
