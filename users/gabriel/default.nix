d6b065c6a9a6d2c481571341350f8c24b1889a6f
Markus Mayr <markus.mayr@outlook.com>
2024-08-04 17:36:00 +0200

    Add some programs for gabriel

--- users/gabriel/default.nix
+++ users/gabriel/default.nix
@@ -5,7 +5,19 @@
   config = {
     programs = {
       firefox.enable = true;
+      thunderbird.enable = true;
     };
+
+    home.packages = with pkgs; [
+      libreoffice
+      bsdgames
+    ];
+
+    accounts.email.accounts."gabriel@marion-mayr.at" =
+      (import ../../bits/util/mail/mkBuki.nix {
+        realName = "Gabriel Mayr";
+        address = "gabriel@marion-mayr.at";
+      }) // { primary = true; };
   };
 }
 
