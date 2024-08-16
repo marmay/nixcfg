31a56a14723d46284be27f0ab6da4a3bbe1ab6b7
Markus Mayr <markus.mayr@outlook.com>
2024-08-16 14:21:30 +0200

    Add dictionaries for markus

--- users/markus/default.nix
+++ users/markus/default.nix
@@ -1,7 +1,20 @@
 { config, lib, pkgs, ... }:
+let
+  aspell = pkgs.aspellWithDicts (ps: with ps; [
+	  de
+	]);
+in
 {
   imports = [ ./hm ./profiles ];
   config = {
     home.file.".background.jpg".source = ./background.jpg;
+    home.packages = [
+      aspell
+    ];
+    programs.gpg.enable = true;
+    services.gpg-agent = {
+      enable = true;
+      pinentryPackage = pkgs.pinentry-rofi;
+    };
   };
 }
