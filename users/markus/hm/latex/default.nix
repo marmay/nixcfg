f575375ab3b2f1b0ea44fe9ec07a28349f3baf90
Markus Mayr <markus.mayr@outlook.com>
2024-08-16 14:21:41 +0200

    Additional LaTeX packages for markus

--- users/markus/hm/latex/default.nix
+++ users/markus/hm/latex/default.nix
@@ -1,9 +1,12 @@
 { config, lib, pkgs, ... }:
 {
   config = {
+    home.packages = with pkgs; [
+      symbola
+    ];
     programs.texlive = {
       enable = true;
-      extraPackages = (tpkgs: { inherit (tpkgs) collection-latex collection-xetex collection-langgerman eurosym fontaxes mathspec moderncv preprint academicons fontawesome5 opensans pgf pgfplots multirow arydshln koma-script enumitem fontspec exam iwona iwonamath xkeyval; });
+      extraPackages = (tpkgs: { inherit (tpkgs) cnltx cntformats collection-latex collection-xetex collection-langgerman eurosym environ emoji exsheets fontaxes mathspec moderncv pgfopts preprint academicons fontawesome5 opensans pgf pgfplots multirow arydshln koma-script enumitem fontspec exam tasks ulem translations iwona iwonamath xkeyval wrapfig; });
     };
   };
 }
