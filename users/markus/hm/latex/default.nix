fbc876b56712da82f5d1d9d280a442b9d330a0c7
Markus Mayr <markus.mayr@outlook.com>
2024-09-14 11:07:32 +0200

    Some latex changes

--- users/markus/hm/latex/default.nix
+++ users/markus/hm/latex/default.nix
@@ -6,7 +6,7 @@
     ];
     programs.texlive = {
       enable = true;
-      extraPackages = (tpkgs: { inherit (tpkgs) background cnltx cntformats collection-latex collection-xetex collection-langgerman eurosym environ emoji everypage exsheets fontaxes mathspec moderncv pdfprivacy pgfopts preprint academicons fontawesome5 opensans pgf pgfplots multirow arydshln koma-script enumitem fontspec exam tasks ulem translations iwona iwonamath xfrac xkeyval wrapfig; });
+      extraPackages = (tpkgs: { inherit (tpkgs) background cnltx cntformats collection-latex collection-xetex collection-langgerman eurosym environ emoji everypage exsheets fontaxes mathspec moderncv ninecolors pdfprivacy pgfopts preprint academicons fontawesome5 opensans pgf pgfplots multirow arydshln koma-script enumitem fontspec exam tasks ulem tabularray translations iwona iwonamath xfrac xkeyval wrapfig; });
     };
   };
 }
