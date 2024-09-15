2659b7a33acaf5670eda94447802ddb0218dd857
Markus Mayr <markus.mayr@outlook.com>
2024-09-15 10:03:24 +0200

    Add TempEl to emacs config

--- users/markus/hm/programs/emacs/default.nix
+++ users/markus/hm/programs/emacs/default.nix
@@ -28,6 +28,7 @@
         epkgs.org
         epkgs.quelpa
         epkgs.quelpa-use-package
+	epkgs.tempel
         epkgs.treemacs
         epkgs.treemacs-evil
         epkgs.use-package
@@ -258,8 +259,44 @@
           (define-key evil-normal-state-map (kbd ", w") 'evil-window-vsplit))
 
         (setq display-line-numbers-type 'relative)
+
+        ;; Configure Tempel
+        (use-package tempel
+          ;; Require trigger prefix before template name when completing.
+          ;; :custom
+          ;; (tempel-trigger-prefix "<")
+        
+          :bind (("M-+" . tempel-complete) ;; Alternative tempel-expand
+                 ("M-*" . tempel-insert))
+        
+          :init
+        
+          ;; Setup completion at point
+          (defun tempel-setup-capf ()
+            ;; Add the Tempel Capf to `completion-at-point-functions'.
+            ;; `tempel-expand' only triggers on exact matches. Alternatively use
+            ;; `tempel-complete' if you want to see all matches, but then you
+            ;; should also configure `tempel-trigger-prefix', such that Tempel
+            ;; does not trigger too often when you don't expect it. NOTE: We add
+            ;; `tempel-expand' *before* the main programming mode Capf, such
+            ;; that it will be tried first.
+            (setq-local completion-at-point-functions
+                        (cons #'tempel-expand
+                              completion-at-point-functions)))
+        
+          (add-hook 'conf-mode-hook 'tempel-setup-capf)
+          (add-hook 'prog-mode-hook 'tempel-setup-capf)
+          (add-hook 'text-mode-hook 'tempel-setup-capf)
+        
+          ;; Optionally make the Tempel templates available to Abbrev,
+          ;; either locally or globally. `expand-abbrev' is bound to C-x '.
+          ;; (add-hook 'prog-mode-hook #'tempel-abbrev-mode)
+          ;; (global-tempel-abbrev-mode)
+        )
       '';
     };
+
+    xdg.configFile.".emacs/templates".source = ./tempel-templates;
   };
 }
 
