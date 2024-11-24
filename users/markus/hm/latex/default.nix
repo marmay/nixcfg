fc83b8486a2942783d6cf88ab4286356f6d15f52
Markus Mayr <markus.mayr@outlook.com>
2024-11-24 15:55:09 +0100

    Secrets aktualisiert, LaTeX sortiert

--- users/markus/hm/latex/default.nix
+++ users/markus/hm/latex/default.nix
@@ -6,7 +6,60 @@
     ];
     programs.texlive = {
       enable = true;
-      extraPackages = (tpkgs: { inherit (tpkgs) animate background biblatex cnltx cntformats collection-latex collection-xetex collection-langgerman eurosym environ emoji everypage exsheets fontaxes framed lastpage makecell mathspec moderncv ninecolors media9 pdfprivacy pgfopts preprint ocgx2 academicons fontawesome5 opensans pgf pgfplots multirow arydshln koma-script enumitem fontspec exam standalone tasks ulem tabularray translations totcount iwona iwonamath xfrac xkeyval xstring wrapfig zref; });
+      extraPackages = (tpkgs: {
+        inherit (tpkgs)
+	  academicons
+	  animate
+	  arydshln
+	  background
+	  biblatex
+	  cnltx
+	  cntformats
+	  collection-langgerman
+	  collection-latex
+	  collection-xetex
+	  emoji
+	  enumitem
+	  environ
+	  eurosym
+	  everypage
+	  exam
+	  exsheets
+	  fontawesome5
+	  fontaxes
+	  fontspec
+	  framed
+	  iwona
+	  iwonamath
+	  koma-script
+	  lastpage
+	  listings
+	  makecell
+	  mathspec
+	  media9
+	  moderncv
+	  multirow
+	  ninecolors
+	  ocgx2
+	  opensans
+	  pdfprivacy
+	  pgf
+	  pgfopts
+	  pgfplots
+	  preprint
+	  standalone
+	  tabularray
+	  tasks
+	  totcount
+	  translations
+	  ulem
+	  wrapfig
+	  xfrac
+	  xkeyval
+	  xstring
+	  zref
+	;
+      });
     };
   };
 }
