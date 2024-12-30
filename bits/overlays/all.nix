a98673935b5434be0740a2133021aed78e358a6b
Markus Mayr <markus.mayr@outlook.com>
2024-12-30 11:09:51 +0100

    Use xmonad-contrib 0.18.1

--- bits/overlays/all.nix
+++ bits/overlays/all.nix
@@ -2,4 +2,5 @@
   (import ./kodi_libretro.nix)
   (import ./kodi.nix)
   (import ./vlc.nix)
+  (import ./xmonad.nix)
 ]
---
+++ bits/overlays/xmonad.nix
@@ -0,0 +1,5 @@
+self: super: {
+  xmonad-contrib = self.xmonad_0_18_1;
+}
+
+
