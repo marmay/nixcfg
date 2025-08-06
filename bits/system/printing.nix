cf4fe6282c4c92e4a72bad4a5d0cc0ca0b18c8c6
Markus Mayr <markus.mayr@outlook.com>
2025-08-06 08:12:41 +0200

    Fix printing

--- bits/system/printing.nix
+++ bits/system/printing.nix
@@ -3,6 +3,17 @@
   options.marmar.printingSupport = lib.mkEnableOption "Printing Support";
 
   config = lib.mkIf config.marmar.printingSupport {
+    hardware.printers = {
+      ensurePrinters = [
+        {
+    	  name = "BR_HL_L3210CW";
+    	  location = "Keller@Zuhause";
+    	  deviceUri = "ipp://BRW4CD577B1CE33.local:631/ipp/print";
+	  model = "everywhere";
+    	}
+      ];
+    };
+	  
     services = {
       avahi = {
         enable = true;
@@ -12,11 +23,11 @@
 
       printing = {
         enable = true;
-	stateless = true;
         browsing = true;
 	browsed.enable = true;
+	openFirewall = true;
         defaultShared = false;
-	drivers = with pkgs; [ gutenprint ];
+	drivers = with pkgs; [ gutenprint brlaser ];
       };
     };
   };
