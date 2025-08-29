5e98f2454e27edbf2fb9a2e7cd0ad95af65a5d97
Markus Mayr <markus.mayr@outlook.com>
2025-08-29 07:11:11 +0200

    Disable ensurePrinters, as it seems to be broken
    CUPS automatically seems to pick up the printer anyway now.

--- bits/system/printing.nix
+++ bits/system/printing.nix
@@ -3,17 +3,6 @@
   options.marmar.printingSupport = lib.mkEnableOption "Printing Support";
 
   config = lib.mkIf config.marmar.printingSupport {
-    hardware.printers = {
-      ensurePrinters = [
-        {
-    	  name = "BR_HL_L3210CW";
-    	  location = "Keller@Zuhause";
-    	  deviceUri = "ipp://BRW4CD577B1CE33.local:631/ipp/print";
-	  model = "everywhere";
-    	}
-      ];
-    };
-	  
     services = {
       avahi = {
         enable = true;
