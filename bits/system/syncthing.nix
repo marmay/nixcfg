4b13243f3a75ddef024d7a92c18c478b95eab576
Markus Mayr <markus.mayr@outlook.com>
2026-05-16 14:51:42 +0200

    Finish syncthing

--- bits/system/syncthing.nix
+++ bits/system/syncthing.nix
@@ -11,16 +11,14 @@
     settings = {
       devices = {
         "mnb" = { id = "N24YM3P-BUDBBAA-KVN6OTS-PLNHFGA-HTMHLPU-QLKDBUC-3DKISCJ-DGWIPQX"; };
+	"keller" = { id = "ZOJN22D-HSOKKZD-HPRBCDI-GVOKI7C-5JYQVPY-7EDCX6V-5B6MOXC-ZILOIA5"; };
 	"phone" = { id = "FGWF2AM-27DJLKG-KLJN7Z2-R7BB2IP-MI3UPGB-6VDSTZ2-QZ3IGLW-HWUF2AZ"; };
       };
-    #     "laptop2" = { id = "HIJKLMN-..."; };
-    #     "phone"   = { id = "OPQRSTU-..."; };
-    #   };
-    #   folders."org" = {
-    #     id = "org-files";                  # muss auf allen Geräten identisch sein
-    #     path = "/home/markus/org";
-    #     devices = [ "laptop1" "laptop2" "phone" ];
-    #   };
+      folders."org" = {
+        id = "org-files";                  # muss auf allen Geräten identisch sein
+        path = "/home/markus/org";
+        devices = [ "mnb" "keller" "phone" ];
+      };
     };
   };
 }
